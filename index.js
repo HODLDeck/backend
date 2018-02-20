62fb7f0ff15c0c6e54c941da6daca8772610f9d7
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-02-20 18:16:33 +0100

    fixes more errors introduced by frontend classes

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express'),
       cookieSession = require('cookie-session'),
       mustacheExpress = require('mustache-express'),
       bodyParser = require('body-parser'),
-      api = require('./src/controllers/api'),
-      frontend = require('./src/controllers/frontend')
+      api = require('./src/controllers/api')
 
 const app = express()
 
 var port = process.env.PORT || 4000
 
 app.use('/api', api)
-app.use('/', frontend)
 
 app.listen(port, function () {
     console.log('listening on port '+port)
