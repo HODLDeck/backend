3db0db784d8664a7e9d39fb60f73c8b463281c5d
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-02-26 23:00:20 +0100

    finishes last line to break reliance on timeout

--- src/controllers/api/devices/index.js
+++ src/controllers/api/devices/index.js
@@ -1,7 +1,7 @@
 var devices = require('../../../models/devices')
 
 exports.post = function(req, res) {
-    res.send(devices.add(req.headers["x-chip-id"], req.headers["x-stellar-wallet"]))
+    res.send(devices.add(req.headers["x-chip-id"], req.headers["x-stellar-wallet"]) + "\r\n")
 }
 
 exports.get = function(req, res) {
