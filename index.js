fcca52c7f9c84d519d216072481a944d9cb5ed0a
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2018-03-13 00:31:55 +0100

    adds basic control interface for merchant

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express'),
+const app = require('express')(),
       cookieSession = require('cookie-session'),
       mustacheExpress = require('mustache-express'),
       bodyParser = require('body-parser'),
-      api = require('./src/controllers/api')
-
-const app = express()
+      api = require('./src/controllers/api'),
+      server = require('http').Server(app),
+      io = require('socket.io')(server);
 
 var port = process.env.PORT || 4000
 
 app.use('/api', api)
-
-app.listen(port, function () {
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+  
+server.listen(port, "127.0.0.1", function () {
     console.log('listening on port '+port)
 })
+
+io.on('connection', function (socket) {
+    // socket.emit('news', { hello: 'world' });
+    // socket.on('my other event', function (data) {
+    //     console.log(data);
+    // });
+});
